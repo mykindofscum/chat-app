a0291f7e07764aa4943dc980894bbe86746760e9
mykindofscum <alvinkchan99@gmail.com>
2019-08-13 16:01:54 -0700

    Add public/index.html

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Welcome to Chat App')
-})
- 
+
+app.use(express.static('public'))
+
 app.listen(3000)
