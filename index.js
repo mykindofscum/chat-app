0f87096beebc009a330abcd5e126bb2a0f28b14b
mykindofscum <alvinkchan99@gmail.com>
2019-08-13 16:37:17 -0700

    Send and acknowledge messages

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded())
 app.use(express.static('public'))
+
+app.post('/message', (req, res) => {
+    res.send('Thank You for message')
+    console.log('received message', req.body.message);
+})
 
 app.listen(3000)
