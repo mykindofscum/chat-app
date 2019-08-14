c8c1384f17d99101a53b4ff6db317dae6964bfc2
mykindofscum <alvinkchan99@gmail.com>
2019-08-13 17:41:04 -0700

    Install ejs, render chat messages

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
+const messages = []
 
 app.use(express.urlencoded())
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('messages.ejs', {
+        messages: messages
+    });
+});
 
 app.post('/message', (req, res) => {
-    res.send('Thank You for message')
     console.log('received message', req.body.message);
+    messages.push(req.body.message)
+    res.redirect('/')
 })
+    
+
+
 
 app.listen(3000)
